e34f3e68fc573926ad4496c2c68cc811141b1cf2
AngelaJiaNan <angelanan12@gmail.com>
2024-09-12 12:13:10 -0700

    finished all functionality for bnt

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    tittle: "My First Book",
+    description: "the first book",
+  },
+  {
+    id: "p2",
+    price: 10,
+    tittle: "My Second Book",
+    description: "the first book",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+          title={product.title}
+          price={product.price}
+          id={product.id}
+          description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
